5a62422260ddb63303f94ca4c0ed71bff7f44539
L00029122 <L00029122@PR2286-01>
2017-10-13 16:32:27 +0100

    class practicals

---
+++ fibonacciSeries/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace fibonacciSeries
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int maxNumber = Convert.ToInt16(tbxMaxNumber.Text.Trim());
+           
+
+            int fibonacciCalculation = 0;
+
+            //list of integers, is a dynamic array that we don't have to define a head of time
+
+            List<int> fibonacciList = new List<int>;
+
+            //defining the starting elements
+            fibonacciList.Add(0);
+            fibonacciList.Add(1);
+
+            for (int arrayposition = 0; arrayposition <= maxNumber; arrayposition++)
+            {
+                int lastElement = fibonacciList[fibonacciList.Count - 1];
+                int seconfLastElement = fibonacciList[fibonacciList.Count - 2];
+                int result = lastElement + seconfLastElement;
+                if (result <= maxNumber)
+                {
+                    fibonacciList.Insert(fibonacciList.Count, result)
+                }
+                fibonacciList.ElementAt(fibonacciList.Last<>)
+
+            }
+        }
+    }
+}
---
+++ loops2/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loops2
+{
+    class Class1
+    {
+        static void Main(String[] args)
+
+        for(int i = 0; i<5; i++)
+        {
+            for(int j = 0; j<25; j++)
+             {
+                Console.Write("*");
+              }
+            Console.WriteLine();
+         }
+        Console.ReadKey();
+
+    }
+}
